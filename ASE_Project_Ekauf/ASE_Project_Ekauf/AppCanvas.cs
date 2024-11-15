011812896713f58a3b2e480e4d48b4001d925f44
ekksttasy <114907880+ekksttasy@users.noreply.github.com>
2024-11-15 11:43:16 +0000

    added appcanvas, shape, and updated form1 code to include aboutboose attempt & texbox initialization.

---
+++ ASE_Project_Ekauf/ASE_Project_Ekauf/AppCanvas.cs
@@ -0,0 +1,57 @@
+﻿using BOOSE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Project_Ekauf
+{
+    /// <summary>
+    /// Class implements ICanvas from Boose to render Boose code on form.
+    /// </summary>
+    public class AppCanvas : ICanvas
+    {
+        private Color pColor;
+        private Pen myPen;
+        private int x_Pos, y_Pos;
+        
+        public Color PenColor
+        {
+            get
+            {
+                return pColor;
+            }
+            set
+            {
+                if (value != pColor)
+                {
+                    pColor = value;
+                }
+            }
+        }
+        public int Xpos
+        {
+            get
+            {
+                return x_Pos;
+            }
+            set
+            {
+                x_Pos = value;
+            }
+        }
+
+        public int Ypos
+        {
+            get
+            {
+                return y_Pos;   
+            }
+            set
+            {
+                y_Pos = value;
+            }
+        }
+    }
+}
--- ASE_Project_Ekauf/ASE_Project_Ekauf/Form1.cs
+++ ASE_Project_Ekauf/ASE_Project_Ekauf/Form1.cs
@@ -1,10 +1,44 @@
+using System.Diagnostics;
+using BOOSE;
+using System.Drawing;
+
+
 namespace ASE_Project_Ekauf
 {
     public partial class Form1 : Form
     {
+        public static class AboutBOOSE : Object
+        {
+            public const int ARRAYLIMIT = 2;
+            public const int BODYLIMIT = 2;
+            public const int COMPOUNCOMMANDLIMIT = 2;
+            public const int MAXPROGRAMSIZE = 10;
+            public const bool RESTRICTIONS = false;
+            public const int SIZELIMIT = 100;
+            public const int VARIABLELIMIT = 5;
+
+            public static float Version
+            {
+                get
+                {
+                    return float value; //HELP LMAO
+                }
+            }
+            public static string about();
+        }
+        
+        public TextBox BooseInput
+        {
+            get;
+            private set;
+        }
         public Form1()
         {
             InitializeComponent();
+            Debug.WriteLine(AboutBOOSE.about());
+            BooseCanvas = new AppCanvas();
+            BooseInput = new TextBox();
+
         }
     }
 }
